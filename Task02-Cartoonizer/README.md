fe02893db06278076b20df67599f9c848fa7fee7
IvicaDuspara <ivicadus@gmail.com>
2020-07-23 19:29:06 +0200

    Created README.md for Task02. Started adding sliders.

---
+++ Task02-Cartoonizer/README.md
@@ -0,0 +1,10 @@
+# Task 02 - Cartoonizer
+Cartoonizes an image. 
+
+[1. Running](#Run)
+
+<a name="Run"></a>
+## Running
+```
+user@username: ~$ ./Task02
+```
--- Task02-Cartoonizer/src/main.cpp
+++ Task02-Cartoonizer/src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <string>
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-cv::Mat cartoonize(const cv::Mat& in, int pyramids=2, int bilateral_passes=4) {
+const std::string res1_path = "../../resources/image1.jpg";
+const std::string res2_path = "../../resources/image2.jpg";
+
+
+cv::Mat cartoonize(const cv::Mat& in, int bilateral_passes=0, int median_blur = 0, int adaptive_block_size = 0) {
+    int pyramids = 2;
     cv::Mat down_sized,filtered;
     for(int i = 0; i < pyramids; i++) {
         cv::pyrDown(in,down_sized);
@@ -13,26 +20,44 @@ cv::Mat cartoonize(const cv::Mat& in, int pyramids=2, int bilateral_passes=4) {
     for(int i = 0; i < pyramids; i++) {
         cv::pyrUp(filtered, filtered);
     }
+
     if(filtered.size != in.size) {
         cv::resize(filtered, filtered, cv::Size{in.cols, in.rows});
     }
     cv::Mat grayscaled;
     cv::cvtColor(in,grayscaled,cv::COLOR_BGR2GRAY);
-    cv::medianBlur(grayscaled,grayscaled,7);
-    cv::adaptiveThreshold(grayscaled,grayscaled,255,cv::ADAPTIVE_THRESH_MEAN_C,cv::THRESH_BINARY,9,2);
+    cv::medianBlur(grayscaled,grayscaled,median_blur);
+    cv::adaptiveThreshold(grayscaled,grayscaled,255,cv::ADAPTIVE_THRESH_MEAN_C,cv::THRESH_BINARY,adaptive_block_size,2);
     cv::cvtColor(grayscaled,grayscaled,cv::COLOR_GRAY2BGR);
+
     cv::Mat cartoon;
     cv::bitwise_and(filtered,grayscaled,cartoon);
     return cartoon;
 }
 
 int main(int argc, char** argv) {
-    cv::Mat in1 = cv::imread("../../resources/image1.jpg");
+    if(argc != 2) {
+        std::cout << "Usage: ./Task02 <path to image>|<1>|<2>";
+        return -1;
+    }
+    cv::Mat in1;
+    std::string temp(argv[1]);
+    if(temp == "1") {
+        in1 = cv::imread(res1_path);
+    }
+    else if(temp == "2") {
+        in1 = cv::imread(res2_path);
+    }
+    else {
+        in1 = cv::imread(temp);
+    }
     cv::Mat out1 = cartoonize(in1);
+    Output_Pack pack(in1,out1,0,0,0);
+    repaint(pack);
     cv::namedWindow("Example1",cv::WINDOW_NORMAL);
     cv::resizeWindow("Example1",1200,800);
-    cv::imshow("Example1",out1);
-    cv::imshow("Og:",in1);
+    cv::imshow("Original",in1);
+    cv::imshow("Cartoonized",out1);
     cv::waitKey(0);
     return 0;
 }
